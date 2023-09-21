ccc5900ca289d6cc5a4648b2e8b3d959f360936b
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-09-21 11:46:08 -0500

    even Nums

---
+++ evenNums.py
@@ -0,0 +1,8 @@
+for i in range(1,101):
+    if i % 2== 0: # this is inaficienct because it checks all the numbers
+        print(i)
+
+# this is the step parameters method
+
+for i in range(2, 101,2):
+    print (i)
