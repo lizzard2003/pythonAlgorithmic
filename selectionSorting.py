565aa049c504a7ab2405c76abd4185c3c17817f2
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-09-21 13:13:36 -0500

    selection

--- selectionSorting.py
+++ selectionSorting.py
@@ -16,3 +16,17 @@ def find_min(xs):
 xs=[3,2, 5, 4, 10, 8, 12, 25]
 min_value= find_min(xs)
 print(f"The minimum value is {min_value} . ")
+
+def selection_sort(xs):
+    for i in range(len(xs)-1):
+        min_index = i
+        for j in range(i+1, len(xs)):
+            if xs[j] <xs[min_index]:
+                min_index = j
+        xs[i], xs[min_index] = xs[min_index], xs[i]
+
+xs=[3,2, 5, 4, 1, 10, 8, 12, 25]
+print(xs)
+selection_sort(xs)
+print(xs)
+print(all(xs[i] <= xs[i +1] for i in range(len(xs)-1)))
